92699690d52bf0081a911347bb5345121717c3e2
shahzadmac <shahzadmirajdin1@ucp.edu.pk>
2020-02-22 15:47:01 +0500

    first

--- user/logout.php
+++ user/logout.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shahzadmiraj
- * Date: 2019-09-15
- * Time: 13:36
- */
+
 session_start();
 session_destroy();
 setcookie('userid',"" , time() - (86400 * 30), "/",$_SERVER["SERVER_NAME"]);
