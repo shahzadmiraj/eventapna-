82c3857c815ad046fbd0924ed49566f8bbc591bf
shahzadmac <shahzadmirajdin1@ucp.edu.pk>
2020-04-14 20:16:42 +0500

    Before Delete

--- company/hallBranches/HallprizeLists.php
+++ company/hallBranches/HallprizeLists.php
@@ -42,8 +42,9 @@ $halldetail=queryReceive($sql);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <link rel="stylesheet" href="../../webdesign/css/complete.css">
     <link rel="stylesheet" href="../../webdesign/css/loader.css">
+    <link rel="stylesheet" href="../../webdesign/css/complete.css">
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
